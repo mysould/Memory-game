975324596b4f5005d60d53910a64ea5f9f048752
souldrb <my@sould.se>
2017-09-27 10:01:27 +0200

    day 3 duplicatephotos

--- src/game.js
+++ src/game.js
@@ -3,29 +3,59 @@ import Card from "./card"
 import Counter from "./counter"
 import './game.css';
 
+const photos = [
+  "/images/cat1.jpg",
+  "/images/cat2.jpg",
+  "/images/cat3.jpg",
+  "/images/cat4.jpg",
+  "/images/cat5.jpg",
+  "/images/cat6.jpg"
+]
+
 class Game extends React.Component {
 
-  state = {
-    cards: [
-      { src: "/images/cat1.jpg" },
-      { src: "/images/cat2.jpg" },
-      { src: "/images/cat3.jpg" }
-    ]
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: this.setupGame()
+    }
+  }
+
+  setupGame = () => {
+    const dubblePhotos = photos.concat(photos)
+    return dubblePhotos.map((url) => ({
+      src: url,
+      isFlipped: false,
+      isMatched: false
+    }))
+
   }
 
 
-  render () {
-      return (
-          <div className="game">
-              <img className="logotype" src="/images/logo.png" />
-              <p>All of the cats have lost their super identical twin.<br/>Can you help find them all?</p>
-              {this.state.cards.map((card) => (
-                <Card src={card.src} />
-              ))}
-              
-          </div>
-      )
+  renderCard = (something) => {
+    return <Card src = {
+      something.src
+    }
+    />
   }
+
+
+  render() {
+    return ( <
+        div className = "game" >
+        <
+        img className = "logotype"
+        src = "/images/logo.png" / >
+        <
+        p > All of the cats have lost their super identical twin. < br / > Can you help find them all ? < /p> {
+        this.state.cards.map(this.renderCard)
+      }
+
+      <
+      /div>
+  )
+}
 }
 
 
